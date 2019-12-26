1deba60f2cfb7be4ed2a7c77223557869786c6e3
Susam <susam.buildings@gmail.com>
2019-12-26 19:01:12 +0300

    add minimal electron setup

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log("inside of index.js");
---
+++ src/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+
+// Храните глобальную ссылку на объект окна, если вы этого не сделаете, окно будет
+// автоматически закрываться, когда объект JavaScript собирает мусор.
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    win.loadFile("src/index.html");
+
+    // win.webContents.openDevTools();
+
+    win.on("closed", () => {
+        win = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (!win) {
+        createWindow();
+    }
+});
