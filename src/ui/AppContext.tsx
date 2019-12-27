2261305684221c2a32d602c57c3937c911785bf6
Susam <susam.buildings@gmail.com>
2019-12-27 15:25:40 +0300

    add app react-context

---
+++ src/ui/AppContext.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Core } from "../core/Core";
+
+export interface IAppContext {
+    core: Core;
+}
+const { Provider, Consumer } = React.createContext<IAppContext>({} as IAppContext);
+
+interface IAppContextProviderProps {
+    core: Core;
+}
+
+export class AppContextProvider extends React.Component<IAppContextProviderProps> {
+    render() {
+        const { core, children } = this.props;
+        return <Provider value={{ core }}>{children}</Provider>;
+    }
+}
+
+export const AppContextConsumer = Consumer;
--- src/ui/AppUI.tsx
+++ src/ui/AppUI.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import { Core } from "../core/Core";
 import "./AppUI.scss"
+import { AppContextProvider } from "./AppContext";
+import { PostListPage } from "./PostListPage/PostListPage";
 
 interface IAppUiProps {
     core: Core;
 }
 
 export const AppUi: React.FC<IAppUiProps> = ({ core }) => {
-    return <h1>The Diary</h1>;
+    return (
+        <AppContextProvider core={core}>
+            <h1>Diary</h1>
+            <PostListPage />
+        </AppContextProvider>
+    );
 };
